c4a60e2fd482114eef8aca09d6ce0526e0ddd26f
mizterwaffles <nhillhouse04@gmail.com>
2025-10-01 09:58:21 -0600

    added an error catch system

--- Program.cs
+++ Program.cs
@@ -45,18 +45,36 @@ for (int i = 3; i >= 0; i--)
 
 }
 
+static void errorCatch(string? guess, int guessNumber)
+{
+    while (guess == null || guess.Length != 4)
+    {
+        Console.Write($"Guess #{guessNumber}: Your guess did not contain 4 letters. Please guess a sequence of 4 lowercase letters with no repeats. ");
+        guess = Console.ReadLine();
+        Console.WriteLine();
+        errorCatch(guess, guessNumber);
+    }
+        foreach (char item in guess)
+    {
+        if (item != 'a' && item != 'b' && item != 'c' && item != 'd' && item != 'e' && item != 'f' && item != 'g')
+        {
+            Console.Write("Your guess contains a character that is not a lowercase letter between 'a' and 'g'. Please guess a sequence of 4 lowercase letters with no repeats. ");
+            guess = Console.ReadLine();
+            Console.WriteLine();
+            errorCatch(guess, guessNumber);
+        }
+    }
+}
 
 //Get guesses
 while (guess != word)
 {
+    Console.WriteLine();
     guessNumber++;
-    Console.Write($"Guess #{guessNumber}: Please guess a sequence of 4 lowercase letters with no repeats.");
+    Console.Write($"Guess #{guessNumber}: Please guess a sequence of 4 lowercase letters with no repeats. ");
     guess = Console.ReadLine();
-    while (guess == null || guess.Length != 4)
-    {
-        Console.Write($"Guess #{guessNumber}: Please guess a sequence of 4 lowercase letters with no repeats.");
-        guess = Console.ReadLine();
-    }
+    errorCatch(guess, guessNumber);
+
 
     int places = 0, correctPosition = 0;
     for (int item = word.Length - 1; item >= 0; item--)
